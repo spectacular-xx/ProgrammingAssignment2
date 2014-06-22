c911fedf4b367b544eca7f149e15d59fb771a81a
spectacular-xx <spectacular200@gmail.com>
2014-06-23 02:15:29 +0300

    Update cachematrix.R
    Commit tries to cache the calculation of the cache instead of doing it every time.
    This should save time and effort.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions will cache the inverse of a matrix so that it will not be calculated each time since
+## getting the inverse is a time consuming operation 
+## The function will check whether the inverse exists before calculating it. If it exists, it is not calculated
 
-## Write a short comment describing this function
+## This function creates a matrix that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
+     matrix <- solve(x) ## the inverse of x
+     matrix
 }
 
 
-## Write a short comment describing this function
+## This function will check if x has an inverse. If so, no need to calculate the inverse
+## else, we 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     ## Return a matrix that is the inverse of 'x'
+     
+     ## if x is not null, just return it
+     if (!is.null(x))
+     {
+          message("getting cached matrix inverse")
+          return (x)
+     }
+     ## else, get the inverse using function solve() and return it
+     else
+     {
+          inverseX <<- makeCacheMatrix(x)
+          return (inverseX)
+     }
+          
 }
